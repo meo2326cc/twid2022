35ab4cf59816ceb561813a4915cf1ccae2127b8f
meo2326cc <cccmsp1@gmail.com>
2022-10-22 16:32:25 +0800

    enhance loading performance

--- tw.js
+++ tw.js
@@ -15,7 +15,7 @@ let com = document.querySelector('#com');
 
 kaohsiung.addEventListener("click", function(){
     com.innerHTML = "<div><h3> <span class='baselinedec'>高雄 </span> </h3></div>" +
-    " <a href='https://docs.google.com/forms/d/e/1FAIpQLSff3LNXg4-VidcNbHm2GYEhxay5FbBYqS-lUu49FkuAf5k_hg/viewform' target='_blank'><div class='twstyle'><div class='twpic' style='background-image:url(img/kao.png);'></div><div><h4>時代力量：林于凱</h4><p>高雄市議員（三民區）</p></div></div></a>";
+    " <a href='https://docs.google.com/forms/d/e/1FAIpQLSff3LNXg4-VidcNbHm2GYEhxay5FbBYqS-lUu49FkuAf5k_hg/viewform' target='_blank'><div class='twstyle'><div class='twpic' style='background-image:url(img/kao.jpg);'></div><div><h4>時代力量：林于凱</h4><p>高雄市議員（三民區）</p></div></div></a>";
     });
 
 chiayi.addEventListener("click", function(){
@@ -28,24 +28,24 @@ com.innerHTML =
 changhua.addEventListener("click", function(){
     com.innerHTML = 
     "<div><h3> <span class='baselinedec'>彰化</span> </h3></div>" +
-    "<a href='https://docs.google.com/forms/d/e/1FAIpQLSdvEGDEWtDc7dQn9ejyinQ5MSV4PIJwDw-GuyZP3iat-ofJ7Q/viewform' target='_blank'><div class='twstyle'><div class='twpic' style='background-image:url(img/changhua.png);'></div><div><h4>時代力量：吳韋達</h4><p>彰化縣議員（彰化、花壇、芬園）</p></div></div></a>";
+    "<a href='https://docs.google.com/forms/d/e/1FAIpQLSdvEGDEWtDc7dQn9ejyinQ5MSV4PIJwDw-GuyZP3iat-ofJ7Q/viewform' target='_blank'><div class='twstyle'><div class='twpic' style='background-image:url(img/changhua.jpg);'></div><div><h4>時代力量：吳韋達</h4><p>彰化縣議員（彰化、花壇、芬園）</p></div></div></a>";
 });
 
 hsinchu.addEventListener("click", function(){
     com.innerHTML =
     "<div><h3> <span class='baselinedec'>新竹</span> </h3></div>" +
-     "<a href='https://docs.google.com/forms/d/e/1FAIpQLSe9epZi-TIbaB6pE6c-P_mCNCQ65lsl8Jts1A_dp7iMxHYoWw/viewform' target='_blank'><div class='twstyle'><div class='twpic' style='background-image:url(img/hsin.png);'></div><div><h4>時代力量：連郁婷</h4><p>新竹縣議員（竹北）</p></div></div></a>" +
-     "<a href='https://docs.google.com/forms/d/e/1FAIpQLScwsjnLvbezN-pOkncrT_GSpVMR72EIkGMRYV1Ad7l4J9uTsQ/viewform' target='_blank'><div class='twstyle'><div class='twpic' style='background-image:url(img/hsin2.png);'></div><div><h4>時代力量：彭致豪</h4><p>新竹縣第八選區（竹東鎮、五峰鄉）縣議員</p></div></div></a>";
+     "<a href='https://docs.google.com/forms/d/e/1FAIpQLSe9epZi-TIbaB6pE6c-P_mCNCQ65lsl8Jts1A_dp7iMxHYoWw/viewform' target='_blank'><div class='twstyle'><div class='twpic' style='background-image:url(img/hsin.jpg);'></div><div><h4>時代力量：連郁婷</h4><p>新竹縣議員（竹北）</p></div></div></a>" +
+     "<a href='https://docs.google.com/forms/d/e/1FAIpQLScwsjnLvbezN-pOkncrT_GSpVMR72EIkGMRYV1Ad7l4J9uTsQ/viewform' target='_blank'><div class='twstyle'><div class='twpic' style='background-image:url(img/hsin2.jpg);'></div><div><h4>時代力量：彭致豪</h4><p>新竹縣第八選區（竹東鎮、五峰鄉）縣議員</p></div></div></a>";
     });
 
 taichung.addEventListener("click", function(){
     com.innerHTML =
     "<div><h3> <span class='baselinedec'>台中</span> </h3></div>" +
      "<a href='https://docs.google.com/forms/d/e/1FAIpQLSfmRYltAr4Yh81UNl-LSlyFMDtKI8hOFj_23SQXkjZAV_-7mg/viewform' target='_blank'><div class='twstyle'><div class='twpic' style='background-image:url(img/chung.jpg);'></div><div><h4>時代力量：傅智恒</h4><p>台中市第四選區（后里豐原）</p></div></div></a>" +
-     "<a href='https://docs.google.com/forms/d/16LYL1n1eD3mbI0POFKeQRyNEk1Azfib1ieraMAHkUAA/viewform?edit_requested=true' target='_blank'><div class='twstyle'><div class='twpic' style='background-image:url(img/chung2.png);'></div><div><h4>時代力量：徐小媚</h4><p>台中市議員（選區：東南）</p></div></div></a>" +
-     "<a href='https://docs.google.com/forms/d/1WdMleth7OyfHhWCjwWt9vsAFFgXcshbD3cnkeQlrdMs/edit' target='_blank'><div class='twstyle'><div class='twpic' style='background-image:url(img/chung3.png);'></div><div><h4>時代力量：鄭勁節</h4><p>台中市議員（選區：潭子、大雅、神岡）</p></div></div></a>" + 
-     "<a href='https://docs.google.com/forms/d/13AWqV_DsfHKDh2mbr1XaQunLz1W1aT86Gc4cFAiGdXo/edit' target='_blank'><div class='twstyle'><div class='twpic' style='background-image:url(img/chung4.png);'></div><div><h4>時代力量：吳佩芸</h4><p>台中市議員（選區：南屯）</p></div></div></a>" + 
-     "<a href='https://docs.google.com/forms/d/e/1FAIpQLScu9SWFOCb3Vfu_JdOC5kA6J-d8CV5QhOrJvxlak78IijvFIw/viewform' target='_blank'><div class='twstyle'><div class='twpic' style='background-image:url(img/chung5.png);'></div><div><h4>時代力量：鄒明諺</h4><p>台中市議員（ 選區：大里、霧峰) </p></div></div></a>";});
+     "<a href='https://docs.google.com/forms/d/16LYL1n1eD3mbI0POFKeQRyNEk1Azfib1ieraMAHkUAA/viewform?edit_requested=true' target='_blank'><div class='twstyle'><div class='twpic' style='background-image:url(img/chung2.jpg);'></div><div><h4>時代力量：徐小媚</h4><p>台中市議員（選區：東南）</p></div></div></a>" +
+     "<a href='https://docs.google.com/forms/d/1WdMleth7OyfHhWCjwWt9vsAFFgXcshbD3cnkeQlrdMs/edit' target='_blank'><div class='twstyle'><div class='twpic' style='background-image:url(img/chung3.jpg);'></div><div><h4>時代力量：鄭勁節</h4><p>台中市議員（選區：潭子、大雅、神岡）</p></div></div></a>" + 
+     "<a href='https://docs.google.com/forms/d/13AWqV_DsfHKDh2mbr1XaQunLz1W1aT86Gc4cFAiGdXo/edit' target='_blank'><div class='twstyle'><div class='twpic' style='background-image:url(img/chung4.jpg);'></div><div><h4>時代力量：吳佩芸</h4><p>台中市議員（選區：南屯）</p></div></div></a>" + 
+     "<a href='https://docs.google.com/forms/d/e/1FAIpQLScu9SWFOCb3Vfu_JdOC5kA6J-d8CV5QhOrJvxlak78IijvFIw/viewform' target='_blank'><div class='twstyle'><div class='twpic' style='background-image:url(img/chung5.jpg);'></div><div><h4>時代力量：鄒明諺</h4><p>台中市議員（ 選區：大里、霧峰) </p></div></div></a>";});
 
 newtaipei.addEventListener("click", function(){
         com.innerHTML = 
